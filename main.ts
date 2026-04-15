443908a487770341433b38ec8f3442bd55db3b72
imadbouteraa <imad.bouteraa@gmail.com>
2026-04-15 09:11:55 +0100

    with two sensors

--- main.ts
+++ main.ts
@@ -2,39 +2,129 @@ let leftSpeed = 0
 let rightSpeed = 0
 let isTurningLeft = false
 let isTurningRight = false
-let baseSpeed = 20
+let baseSpeed = 17
 let baseCorrection = 10
 let correction = 0
-let correctionStep = 10
+let correctionStep = 5
 let minimumSpeed = 10
-let maximumSpeed = 50
+let maximumSpeed = 35
+let isRunning = false
+let initiated = false
 
+//     block="● ●" enumval=0
+//     Tracking_State_0,
 
-function turnLeft(){
-    correction = (correction < 0)?baseCorrection:correction + correctionStep
+//     block="● ◌" enumval=1
+//     Tracking_State_1,
+
+//     block="◌ ●" enumval=2
+//     Tracking_State_2,
+
+//     block="◌ ◌" enumval=3
+//     Tracking_State_3
+
+function turnLeft(step:number){
+    correction = (correction < 0)?baseCorrection:correction + step
     neZha.setMotorSpeed(neZha.MotorList.M4, Math.min(maximumSpeed, baseSpeed+correction))
     neZha.setMotorSpeed(neZha.MotorList.M1, Math.max(minimumSpeed,baseSpeed-correction))
 }
 
-function turnRight(){
-    correction = (correction > 0) ? - baseCorrection : correction - correctionStep
+function turnRight(step:number){
+    correction = (correction > 0) ? - baseCorrection : correction - step
     neZha.setMotorSpeed(neZha.MotorList.M4, Math.max(minimumSpeed,baseSpeed + correction))
     neZha.setMotorSpeed(neZha.MotorList.M1, Math.min(maximumSpeed, baseSpeed - correction))
 }
 
+function is0000(): boolean{
+    return PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.TrackingStateType.Tracking_State_3) && PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.TrackingStateType.Tracking_State_3)
+}
 
+function is1111(): boolean {
+    return PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.TrackingStateType.Tracking_State_0) && PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.TrackingStateType.Tracking_State_0)
+}
+
+function is0110(): boolean {
+    return PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.TrackingStateType.Tracking_State_1) &&
+     PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.TrackingStateType.Tracking_State_2)
+}
+
+function is0011(): boolean {
+    return PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J1,
+     PlanetX_Basic.TrackingStateType.Tracking_State_0) &&
+        PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J2,
+         PlanetX_Basic.TrackingStateType.Tracking_State_3)
+}
+
+function is0001(): boolean {
+    return PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J1,
+        PlanetX_Basic.TrackingStateType.Tracking_State_2) &&
+        PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J2,
+            PlanetX_Basic.TrackingStateType.Tracking_State_3)
+}
+
+function is1100(): boolean {
+    return PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J1,
+        PlanetX_Basic.TrackingStateType.Tracking_State_3) &&
+        PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J2,
+            PlanetX_Basic.TrackingStateType.Tracking_State_0)
+}
+
+function is1000(): boolean {
+    return PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J1,
+        PlanetX_Basic.TrackingStateType.Tracking_State_3) &&
+        PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J2,
+            PlanetX_Basic.TrackingStateType.Tracking_State_1)
+}
 
 basic.forever(function () {
-    if (PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.TrackingStateType.Tracking_State_1)) {
-        turnLeft()
-    }
-    if (PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.TrackingStateType.Tracking_State_2)) {
-        turnRight()
-    }
-    if (PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.TrackingStateType.Tracking_State_0)) {
-        neZha.setMotorSpeed(neZha.MotorList.M1, 20)
-        neZha.setMotorSpeed(neZha.MotorList.M4, 20)
+    if(isRunning){
+    if (is0001()) {
+        turnRight(2*correctionStep)
+    }else if(is0011()){
+        turnRight(correctionStep)
+    }else if(is0110()){
+        // if(Math.abs(correction)>3*correctionStep){
+
+        //     if(correction>0){
+        //         turnRight(0.1*correctionStep)
+        //     }else 
+        //         turnLeft(0.1*correctionStep)
+        // }
+        neZha.setMotorSpeed(neZha.MotorList.M1, baseSpeed)
+        neZha.setMotorSpeed(neZha.MotorList.M4, baseSpeed)
         correction = 0
+    }else if (is1100()) {
+        turnLeft(correctionStep)
+    } else if (is1000()) {
+        turnLeft(2*correctionStep)
+    } else if(is1111() ){
+        pause(100)
+        if (is1111()){
+        neZha.stopAllMotor()
+        isRunning = false;
+        initiated = false;
+        music.stopAllSounds() 
+        }       
+    }
+    } else if (is0110() && initiated){
+        pause(1000)
+        if(is0110()){
+        neZha.setMotorSpeed(neZha.MotorList.M1, baseSpeed)
+        neZha.setMotorSpeed(neZha.MotorList.M4, baseSpeed)
+        correction = 0
+        isRunning = true
+        pause(10)
+        // control.inBackground(function () {
+        //     while (isRunning) {
+        //         music.playTone(800, 200)  // High tone
+        //         music.playTone(400, 200)  // Low tone
+        //     }
+        // })
+        }
+    } else if(!initiated && is0000() && !isRunning){
+        initiated = true
+        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+        pause(1000)
     }
     pause(10)
 })
